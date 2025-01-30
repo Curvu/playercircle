43913cae2d8902878d952cbf0bfc64041ba44e09
letsid <paul.kottwitz@gmail.com>
2025-01-30 14:36:49 +0100

    first commit

---
+++ PlayerCircle.cs
@@ -0,0 +1,27 @@
+﻿using ExileCore2;
+using ExileCore2.Shared.Nodes;
+using System;
+
+namespace PlayerCircle;
+
+public class PlayerCircle : BaseSettingsPlugin<PlayerCircleSettings>
+{
+    public override void Render()
+    {
+        if (!Settings.Enable || !Settings.ShowCircle) return;
+
+        if (!Settings.Render.ShowInTown && GameController.Area.CurrentArea.IsTown) return;
+        if (!Settings.Render.ShowInHideout && GameController.Area.CurrentArea.IsHideout) return;
+
+        var playerPos = GameController.Player.Pos; // Get the player's world position
+        float radius = Settings.CircleRadius; // Circle radius in world units
+
+        Graphics.DrawCircleInWorld(
+            playerPos, 
+            radius, 
+            Settings.Render.CircleColor, 
+            Settings.CircleThickness, 
+            50 // Number of segments for smoothness
+        );
+    }
+}
---
+++ PlayerCircleSettings.cs
@@ -0,0 +1,35 @@
+﻿using ExileCore2.Shared.Attributes;
+using ExileCore2.Shared.Interfaces;
+using ExileCore2.Shared.Nodes;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace PlayerCircle;
+
+public class PlayerCircleSettings : ISettings
+{
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+
+    [Menu(null, "Enable/Disable circle around player")]
+    public ToggleNode ShowCircle { get; set; } = new ToggleNode(true);
+
+    [Menu(null, "Circle radius around player")]
+    public RangeNode<int> CircleRadius { get; set; } = new RangeNode<int>(1275, 0, 5000);
+
+    [Menu(null, "Number of circle segments (higher = smoother)")]
+    public RangeNode<int> CircleThickness { get; set; } = new RangeNode<int>(1, 2, 20);
+
+    public CircleRender Render { get; set; } = new CircleRender();
+}
+
+[Submenu]
+public class CircleRender
+{
+    [Menu(null, "Show circle in town areas")]
+    public ToggleNode ShowInTown { get; set; } = new ToggleNode(false);
+
+    [Menu(null, "Show circle in hideout")]
+    public ToggleNode ShowInHideout { get; set; } = new ToggleNode(true);
+
+    public ColorNode CircleColor { get; set; } = Color.FromArgb(127, 255, 0, 0);
+}
